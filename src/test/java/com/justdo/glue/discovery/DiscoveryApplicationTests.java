26bf8238afedf57a1df4faf83da2d1c3dc7d9948
gyeom <rlagusrua3687@gmail.com>
2024-04-21 16:24:01 +0900

    :tada: chore add discovery project

---
+++ src/test/java/com/justdo/glue/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justdo.glue.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
